96b8d40fa0ad21352d59f13b632de4d0159a95b2
Tejasvi Mehra <tejasvi.mehra@gmail.com>
2019-02-18 01:31:47 +0800

    Login and Upload file complete

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 import { Camera } from '@ionic-native/camera';//import in app.module.ts
+import { ImagePicker } from '@ionic-native/image-picker';
 
 import { MyApp } from './app.component';
 import { LoginPage } from '../pages/login/login';
@@ -48,6 +49,7 @@ timestampsInSnapshots: true
     SplashScreen,
     
     Camera,
+    ImagePicker,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
--- src/pages/feed/feed.ts
+++ src/pages/feed/feed.ts
@@ -5,6 +5,8 @@ import moment, { duration } from 'moment';
 import { isDifferent } from '@angular/core/src/render3/util';
 import { LoginPage } from '../login/login';
 import { Camera, CameraOptions } from '@ionic-native/camera';
+import { ImagePicker } from '@ionic-native/image-picker';
+
 
 
 @Component({
@@ -17,9 +19,10 @@ export class FeedPage {
   pageSize: number= 10;
   cursor: any;//documentSnapshot- holds value of pageSizeth post
   infiniteEvent: any;
+  image: string; 
 
   constructor(public navCtrl: NavController, public navParams: NavParams,public loadingCtrl: LoadingController, 
-    public toastCtrl: ToastController, private camera: Camera) {
+    public toastCtrl: ToastController, private camera: Camera, private imagePicker: ImagePicker) {
     this.getPosts();
   }
 
@@ -88,7 +91,12 @@ export class FeedPage {
       owner: firebase.auth().currentUser.uid,
       owner_name: firebase.auth().currentUser.displayName
     }).then((doc) => {
-      console.log(doc)
+      console.log(doc);
+
+      if(this.image){
+        this.upload(doc.id)
+      }
+
       this.text="";
       let toast= this.toastCtrl.create({
         message: "Your post has been created successfully",
@@ -119,9 +127,25 @@ export class FeedPage {
 
   }
 
+  
   addPhoto(){
     this.launchCamera();
+    // this.launchGallery();
   }
+
+  launchGallery(){
+    let options ={
+      maximumImagesCount: 1,
+      quality: 100,
+      width: 512,
+      height: 512,
+      outputType: 1 // Base64
+    }
+    this.imagePicker.getPictures(options).then(function (results){
+        this.image = "data:image/png;base64," + results[0];
+  }, (err) => { console.log('Error') });
+  }
+
 
 launchCamera(){
   let options: CameraOptions = {
@@ -133,12 +157,30 @@ launchCamera(){
     correctOrientation: true,
     targetHeight: 512,
     targetWidth: 512,
-    allowEdit: true
+    allowEdit: true,
+    // sourceType: this.camera.PictureSourceType.PHOTOLIBRARY
   }
   this.camera.getPicture(options).then((base64Image)=>{
-
-    console.log(base64Image)
+    this.image = "data:image/png;base64," + base64Image;
+    // console.log(base64Image)
   }).catch((err)=>{console.log(err)})
+}
+
+upload(name: string){
+  let ref = firebase.storage().ref("postImages/" + name);
+  let uploadTask = ref.putString(this.image.split(',')[1], "base64");
+  uploadTask.on("state_changed", function(taskSnapshot){
+    console.log(taskSnapshot);
+  }, function(err){
+    console.log(err);
+  }, function(){
+    console.log("Upload Complete");
+    uploadTask.snapshot.ref.getDownloadURL().then(function(url){
+      console.log(url);
+      
+    })
+    
+  })
 }
 
 }
