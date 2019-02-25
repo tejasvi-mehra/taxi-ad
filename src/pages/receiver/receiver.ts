345689af8850858c46ee9da61755657751c0cd2a
Tejasvi Mehra <tejasvi.mehra@gmail.com>
2019-02-25 16:52:41 +0800

    subscribe complete

--- src/pages/receiver/receiver.ts
+++ src/pages/receiver/receiver.ts
@@ -20,7 +20,8 @@ import { ScreenOrientation } from '@ionic-native/screen-orientation';
   templateUrl: 'receiver.html',
 })
 export class ReceiverPage {
-  retrieved_images: string[]=[];
+  retrieved_images: string[] = [];
+  TIMEOUT_INTERVAL:number = 1000 * 10 ;
   all_locations: any;
   today: string = new Date().toISOString(); // minimum date = current date
   startDate: string = new Date().toISOString();
@@ -29,9 +30,10 @@ export class ReceiverPage {
 
   constructor(public navCtrl: NavController, public navParams: NavParams, private screenOrientation: ScreenOrientation) {
     this.get_all_locations().then(()=>{
-      console.log("Locations loaded");        
+      console.log("Locations loaded");
+      this.subscribe_location();
     });
-    this.screenOrientation.lock(this.screenOrientation.ORIENTATIONS.LANDSCAPE);
+    // this.screenOrientation.lock(this.screenOrientation.ORIENTATIONS.LANDSCAPE);
   }
 
     // Set up all locations from DB
@@ -64,8 +66,13 @@ export class ReceiverPage {
     });
   }
 
+
+  // TODO - Receive permission
+
   getlocation(){
     let me = this;
+    console.log("here 0");
+    
     this.getlocation_wrapper().then((resp:any)=>{
       // console.log(typeof this.all_locations[0].point);
       let latitude = resp.coords.latitude;
@@ -74,23 +81,30 @@ export class ReceiverPage {
       result = geolib.findNearest({ latitude: latitude, longitude: longitude }, this.all_locations);
       console.log("Closest to:");        
       console.log(this.all_locations[result.key]);
+      console.log("here 1")
       let current_time = new Date();
       let current_location =  (this.all_locations[result.key]).value;
-      firebase.firestore().collection('file_data').where('location','==',current_location).get().then(function(result){
-        let value = result.docs.map ((doc) => { return {data:doc.data(), id:doc.id} });
+      firebase.firestore().collection('file_data').where('location', '==', current_location).get().then(function (result) {
+        console.log("here 2")
+        let value = result.docs.map((doc) => { return { data: doc.data(), id: doc.id } });
+        console.log("here 3")
         // console.log("values:");
         value = value.filter((val)=>{        
           return (new Date(val.data.startDate) < current_time) && (current_time < new Date(val.data.endDate) );
         })
+        let temp_images = [];
+        // me.retrieved_images = [];
+        console.log("here 4")
         value.forEach((val:any) => {
           // console.log(val.id);        
           // console.log(val.data.location);
           // console.log(val.data.startDate);
-          // console.log(val.data.endDate);
-          me.retrieved_images.push(val.data.image_url);        
+          console.log(val.data.image_url);
+          temp_images.push(val.data.image_url);        
           // me.download(val.id);
           
         })
+        me.retrieved_images = temp_images;
   
         // me.slideshow()
         
@@ -103,6 +117,13 @@ export class ReceiverPage {
       // this.all_locations = value;
       
     })   
+  }
+
+  subscribe_location() {
+    this.getlocation();
+    setInterval(() => {
+      this.getlocation()
+    }, this.TIMEOUT_INTERVAL);
   }
 
   
